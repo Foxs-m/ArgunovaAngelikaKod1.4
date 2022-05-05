e6c78696654de8abfd52bfa3c2b85e39d3f9aa4f
Admin <Admin@DESKTOP-PHKI08E>
2022-05-05 15:34:01 +0900

    Добавьте файлы проекта.

---
+++ kodpage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArgunovaAngelikaKod1._4
+{
+    /// <summary>
+    /// Логика взаимодействия для kodpage.xaml
+    /// </summary>
+    public partial class kodpage : Page
+    {
+        public kodpage()
+        {
+            InitializeComponent();
+            DGridKod.ItemsSource = Entities.GetContext().Agent.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
